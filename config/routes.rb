6ddaf1760f37267f1a48bda37b4f12b417842a7d
101415 <j.chicharito.h.0014@gmail.com>
2020-02-20 19:51:51 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweet#top_page'
-  # resources :tweets, only: :show
-  get "users/:id" => "tweets#show", as: :tweets
+  resources :tweet, only: :show
 end
